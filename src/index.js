90f948f96bf5c7419bb6e28bab23f661a5b365af
DCT <mirachan2332@gmail.com>
2022-04-19 10:20:46 -0500

    set up React within index.js in src folder

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// main entry for application
+import React from "react";
+
+// renders App at root element of index.html
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+reportWebVitals();
