1adcab0a91fa2508cb5465bfb5425112f8b9577c
DCT <mirachan2332@gmail.com>
2022-04-19 11:11:41 -0500

    Header.js updated for banner image placeholder and name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,4 +3,12 @@ import React from "react";
 import headerImg from "../assets/images/headerImg.png";
 import '../index.css';
 
+function Header(props) {
+    return (
+        <header className="my-5">
+            <img src={headerImg} alt="placeholderAltTextOfMyHeaderImg" />
+            <h2> Diana Taylor</h2>
+        </header>
+    );
+}
 export default Header;
