b452034bc7f8b469d1d7f4443dc2d1bfd38119da
DCT <mirachan2332@gmail.com>
2022-04-19 12:24:28 -0500

    added imports for PortfolioContainer; renamed NavTabs as NavBar, removed old NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,55 @@
 import React from "react";
 import '../index.css';
 
-function NavBar() {
-    return <header data-testid="header" className="flex-row px-1"></header>
+function NavBar({ currentPage, handlePageChange }) {
+    return (
+        <header>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <a
+                        href="#aboutMe"
+                        onClick={() => handlePageChange("AboutMe")}
+                        className={currentPage === "AboutMe" ? "nav-link active" : "nav-link"}
+                    >
+                        About Me
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#resume"
+                        onClick={() => handlePageChange("Resume")}
+                        className={
+                            currentPage === "Resume" ? "nav-link active" : "nav-link"
+                        }
+                    >
+                        Resume
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#portfolio"
+                        onClick={() => handlePageChange("Portfolio")}
+                        className={
+                            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+                        }
+                    >
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#contactInfo"
+                        onClick={() => handlePageChange("ContactInfo")}
+                        className={
+                            currentPage === "Contact" ? "nav-link active" : "nav-link"
+                        }
+                    >
+                        Contact Info
+                    </a>
+                </li>
+            </ul>
+        </header>
+    );
 }
 
 export default NavBar;
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,3 +1,10 @@
 import React from "react";
+import AboutMe from "./AboutMe";
+import ContactInfo from "./Contact";
+import Resume from "./Resume";
+import Portfolio from "./Portfolio";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default PortfolioContainer;
