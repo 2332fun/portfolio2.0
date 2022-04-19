b53771f8f74f0d341bda4c71ec1bc5a4c7885305
DCT <mirachan2332@gmail.com>
2022-04-19 10:30:04 -0500

    major folder fix; react installed properly this time

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-// App.js is root component: to change the application; modify file or add components in it.
+import logo from './logo.svg';
+import './App.css';
 
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
