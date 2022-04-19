0b6de9476b7a7eae78792937166e268ce67c9f12
DCT <mirachan2332@gmail.com>
2022-04-19 10:47:56 -0500

    AboutMe.js basic html layout setup

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,4 +3,26 @@ import React from "react";
 import myImage from "../assets/images/myImage.png";
 import '../index.css';
 
+function AboutMe() {
+    return (
+        // class my-5 = (m) margin (y) top & bottom - (5) $spacer-y * 3
+        <section className="my-5">
+            <h1 id="aboutMe">About Me</h1>
+            {/* Recent photo and short bio */}
+            <img
+                class="photo"
+                src={myImage}
+                className="photo"
+                style={{ width: "25" }}
+                alt="Diana Taylor's physical profile"
+            />
+            <div className="my-2" id="summary">
+                <p>
+                    AboutMeParagraphPlaceholder
+                </p>
+            </div>
+        </section>
+    );
+}
+
 export default AboutMe;
