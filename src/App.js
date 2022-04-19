d94f40f95a6cbf46e7c63b67fdb4b51c5b289c12
DCT <mirachan2332@gmail.com>
2022-04-19 10:14:07 -0500

    filled out App.js based on lecture info

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// App.js is root component: to change the application; modify file or add components in it.
+
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+const App = () => <PortfolioContainer />;
+
+export default App;
