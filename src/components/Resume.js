3789d37572aa6c38bec975fc63963bb92194af33
DCT <mirachan2332@gmail.com>
2022-04-19 12:18:23 -0500

    Resume.js set up

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,5 +2,32 @@ import React from "react";
 import pdf from "../assets/small/resume.pdf";
 import '../index.css';
 
+function Resume() {
+    return (
+        <section className="my-5">
+            <h1 id="resume">Resume</h1>
+            <div className="my-5" id="skillset">
+                <ul>
+                    <li>HTML / CSS / Javascript</li>
+                    <li>Git</li>
+                    <li>MERN Stack</li>
+                    <li>SQL / NoSQL</li>
+                    <li>Group Projects: Back End and Full Stack</li>
+                </ul>
+            </div>
+            <div>
+                <a
+                href={pdf}
+                id="pdf"
+                className="my-4"
+                target="_blank"
+                rel="noreferrer"
+                >
+                    Resume Download (PDF)
+                </a>
+            </div>
+        </section>
+    );
+}
 
 export default Resume;
