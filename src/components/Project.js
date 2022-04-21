52f6a4f633b9db612463af8c49bbedd65b297be1
DCT <mirachan2332@gmail.com>
2022-04-21 16:17:45 -0500

    setup src/components/Project.js to display prop based projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,15 @@
 import React from "react";
+import '../index.css';
+
+const Project = (props) => {
+    return (
+        <>
+            <div>{props.title}</div>
+            <a href={props.href}>
+                <img src={props.image} className="projects" alt=""></img>
+            </a>
+        </>
+    );
+}
 
 export default Project;
