6d42eadd0651bee610be633a7ff6900a59006580
DCT <mirachan2332@gmail.com>
2022-04-21 16:00:59 -0500

    imported project and images into src/components/Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,3 +1,13 @@
 import React from "react";
+import Project from "./Project";
+import weatherImg from "../assets/images/CityWeather.png";
+import trackImg from "../assets/images/EmployeeTrackDB.png";
+import headerImg from "../assets/images/headerImg.png";
+import musicImg from "../assets/images/music-dashboard.png";
+import myImg from "../assets/images/myImage.png";
+import puppyImg from "../assets/images/Puppy-Love.png";
+import socialImg from "../assets/images/Socially-NoSQL.png";
+import workImg from "../assets/images/WorkDayScheduler.png";
+
 
 export default Portfolio;
