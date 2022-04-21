0edbe8df5688af95e022dcf0a478e2462bd26233
DCT <mirachan2332@gmail.com>
2022-04-21 16:22:24 -0500

    footer.js set up to display gitHub and linkedIn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,24 @@
 import React from "react";
+import '../index.css';
+
+function Footer() {
+    return (
+        <footer>
+            <section>
+                <li>
+                    <a href="https://github.com/2332fun" className="gitHub">
+                        gitHub
+                    </a>
+                </li>
+                <br></br>
+                <li>
+                    <a href="https://www.linkedin.com/in/diana-taylor-256638a2/" className="linkedIn">
+                        linkedIn
+                    </a>
+                </li>
+            </section>
+        </footer>
+    );
+}
 
 export default Footer;
